af79b2f37f3714d5867f50db8ac91256dd8310b9
tombleron <Tombleron>
2020-05-02 17:02:30 +0700

    Небольшой рефакторинг

--- src/primitives.rs
+++ src/primitives.rs
@@ -1,35 +1,35 @@
 use super::linear::Vector;
 
-/// Sphere object
-///
-/// Round, cool, shinny 
 pub struct Sphere {
-    /// Radius of the sphere from center to edge
     pub radius: u32,
-    /// Object id
     pub id: u32,
-    /// Position of sphere in the world
-    pub center: Vector<f64>
+    pub center: Vector<f64>,
 }
 
 impl Sphere {
-    /// Creates a new Sphere object
-    ///
-    /// # Example
-    ///
-    /// Create new sphere with radius of 5 and positioned at [0, 0, 0] 
-    /// ```
-    /// let point: Vector = Vector::new(0.0,0.0,0.0);
-    /// let sphere: Sphere = Sphere::new(5, point);
-    /// ```
     pub fn new(radius: u32, point: Vector<f64>, id: u32) -> Sphere {
         Sphere{radius: radius, center: point, id: id}
     }
-
     pub fn sdf(&self, point: &Vector<f64>) -> f64 {
         ((self.center.x - point.x).powi(2) + 
          (self.center.y - point.y).powi(2) +
          (self.center.z - point.z).powi(2)).sqrt() - self.radius as f64
     }
 
+}
+
+pub struct Ray {
+    pub direction: Vector<f64>,
+    pub position: Vector<f64>,
+}
+ 
+impl Ray {
+
+    pub fn new(direction: Vector<f64>, position: Vector<f64>) -> Ray {
+        Ray{direction: direction, position: position}
+    }
+    
+    pub fn normalize(&mut self) {
+        self.direction = self.direction.normalize();
+    }
 }
--- src/scene.rs
+++ src/scene.rs
@@ -1,48 +1,7 @@
 use super::linear::Vector;
-use super::primitives::Sphere;
+use super::primitives::{Sphere, Ray};
 use image::{RgbImage};
 
-/// Ray object
-///
-/// It's a bird, it's a plane... No, it's a Ray casting through Scene
-pub struct Ray {
-    /// Dirrection Ray points to
-    direction: Vector<f64>,
-    /// Current position of Ray in the Scene
-    position: Vector<f64>,
-}
- 
-impl Ray {
-    /// Creates a new Ray
-    /// 
-    /// # Example
-    ///
-    /// Create new Ray facing along X axis at [0,0,0]
-    /// ```
-    /// let position: Vector = Vector::new(0.0,0.0,0.0);
-    /// let direction: Vector = Vector::new(1.0,0.0,0.0);
-    /// let ray: Ray = Ray::new(direction, position);
-    /// ```
-    pub fn new(direction: Vector<f64>, position: Vector<f64>) -> Ray {
-        Ray{direction: direction, position: position}
-    }
-    
-    /// Normalizes Ray
-    /// 
-    /// # Example
-    ///
-    /// Normalize ray
-    /// ```
-    /// let position: Vector = Vector::new(0.0,0.0,0.0);
-    /// let direction: Vector = Vector::new(1.0,0.0,0.0);
-    /// let ray: Ray = Ray::new(direction, position);
-    /// ray.normalize();
-    /// assert_eq!(ray.len(), 1.0);
-    /// ```
-    pub fn normalize(&mut self) {
-        self.direction = self.direction.normalize();
-    }
-}
 
 pub struct Camera {
     position: Vector<f64>,
@@ -64,7 +23,7 @@ pub struct Scene {
 }
 
 impl Scene {
-    
+
     pub fn new(spheres: Vec<Sphere>, light: Vec<Vector<f64>>, time: f64, camera: Camera) -> Scene {
         Scene{spheres: spheres, light: light,camera: camera, time: time}
     }
@@ -78,9 +37,6 @@ impl Scene {
         let mut min: f64 = 0.0;
         let mut id: u32 = 0;
         
-        
-        let displace: f64 = (2.0 * point.x + self.time).sin() * (2.0 * point.y + self.time).sin() * (2.0 * point.z + self.time).sin() * 0.25;
-
         for sphere in self.spheres.iter() {
             if count == 0 {
                 min = sphere.sdf(&point);
@@ -91,7 +47,7 @@ impl Scene {
             }
             count += 1;
         }
-        (min + displace, id)
+        (min, id)
     }
 
     fn distance_to_closest_offset(&self, point: &Vector<f64>, offset: u32) -> (f64, u32) {
@@ -99,8 +55,6 @@ impl Scene {
         let mut min: f64 = 1.0;
         let mut id: u32 = 0;
         
-        //let displace: f64 = (5.0 * point.x).sin() * (5.0 * point.y).sin() * (5.0 * point.z).sin() * 0.25;
-
         for sphere in self.spheres.iter() {
             if sphere.id == offset {continue}
             if count == 0 {
@@ -118,11 +72,10 @@ impl Scene {
     
     pub fn fragment(&self, uv: Vector<f64>) -> Vector<f64> {
     
-        let ro: Vector<f64> = Vector::new(0.0,0.0,0.0);
+        let ro: Vector<f64> = self.camera.position;
         
         let mut ray: Ray = Ray::new(uv, ro);
         ray.normalize();
-        //print!("{:.2} ", ray.direction.z);
         let color: Vector<f64> = self.ray_march(&ray);
         color
     }
@@ -137,11 +90,11 @@ impl Scene {
             let x: f64 = x as f64;
             let y = y as f64;
 
-
             let uv: Vector<f64> =  Vector::new(
                                     (2.0 * x - resx)/resx,
                                     (2.0 * y - resy)/resy,
                                     1.0);
+
             let color: Vector<f64> = self.fragment(uv) * 255.0;
             *pixel = image::Rgb([color.x as u8, 
                                  color.y as u8, 
@@ -167,34 +120,8 @@ impl Scene {
             
             if distance_to_closest < min_hit_distance {
 
-                let mut normal = self.calculate_normal(curent_position);
-                //normal = normal * 0.5 + Vector::new(0.5,0.5,0.5);
-                
-                let mut light: f64 = 1.0;
-                let mut light_angle: f64 = 0.0;
+                let (light, light_angle) = self.light_process(curent_position, id);
 
-                for light_source in self.light.iter() {
-                    
-                    let mut direction: Vector<f64> = curent_position - *light_source;
-                    let distance: f64 = direction.len();
-                    direction = direction.normalize();
-                    let dot: f64 = normal.dot(&direction);
-
-                    if dot <= 0.0 {
-                        continue
-                    }
-
-                    if dot > light_angle {
-                        light_angle = dot;
-                    }
-
-                    let result: f64 = self.cast_light(curent_position, direction, distance, id);
-
-                    if result < light {
-                        light = result
-                    }
-                }
-               
                 return Vector::new(1.0,0.0,0.0) * light_angle * light 
             }
 
@@ -207,12 +134,43 @@ impl Scene {
         color
     }
 
+    fn light_process(&self, start_position: Vector<f64>, id: u32) -> (f64, f64) {
+
+        let mut normal = self.calculate_normal(start_position);
+                
+        let mut light: f64 = 0.0;
+        let mut light_angle: f64 = 0.0;
+
+        for light_source in self.light.iter() {
+
+            let mut direction: Vector<f64> = start_position - *light_source;
+            let distance: f64 = direction.len();
+            direction = direction.normalize();
+            let dot: f64 = normal.dot(&direction);
+
+            if dot <= 0.0 {
+                continue
+            }
+
+            if dot > light_angle {
+                light_angle = dot;
+            }
+
+            let result: f64 = self.cast_light(start_position, direction, distance, id);
+
+            if result > light {
+                light = result
+            }
+        }
+        return (light, light_angle)
+    }
+
     pub fn cast_light(&self, start_position: Vector<f64> ,direction: Vector<f64>, distance: f64, id: u32) -> f64 {
 
         let min_hit_distance: f64 = 0.0001;
         
         let mut total_distance: f64 = 0.0;
-        let direction: Vector<f64> = Vector::new(0.0,0.0,0.0)-direction.normalize();
+        let direction: Vector<f64> = Vector::new(0.0,0.0,0.0) - direction.normalize();
                
         let mut min_dif: f64 = 1.1;
 
